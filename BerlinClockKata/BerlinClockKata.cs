e70550085807918fc3814ad3c7dffcf3b647258b
mtshark <mtschark@yahoo.com>
2013-03-14 07:59:51 -0400

    Initial Submit

---
+++ BerlinClockKata/BerlinClockKata.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BerlinClockKata {
+    public class BerlinClockKata {
+        static void Main() {
+            string message = "Please enter a time (hh:mm:ss) ";
+            string enterTime = "Starting";
+            while (enterTime != null && !enterTime.ToUpper().Equals("Q")) {
+                Console.Write(message);
+                enterTime = Console.ReadLine();
+                Console.WriteLine(HandleInput(enterTime));
+                message = "Please enter a time (hh:mm:ss) or Q to quit ";
+            }
+        }
+
+        public static string HandleInput(string pEnterTime) {
+            try {
+                if (pEnterTime.Length != 8)
+                    throw new Exception("Please enter a time in the hh::mm:ss format ");
+                return TranslateToBerlinClock(pEnterTime);
+            }
+            catch (Exception e) {
+                return e.Message;
+            }
+        }
+
+        public static string TranslateToBerlinClock(string pEnterTime) {
+            int[] timeSegments = ParseTime(pEnterTime);
+            StringBuilder returnTime = new StringBuilder();
+            returnTime.AppendLine(GetSecondLight(timeSegments[2]));
+            returnTime.AppendLine(GetHourLights(timeSegments[0]));
+            returnTime.Append(GetMinuteLights(timeSegments[1]));
+
+            return returnTime.ToString();
+        }
+
+        public static StringBuilder FormatTopRow(int topRow) {
+            char[] topRowChar = SetLights(topRow, 11, 'Y').ToArray();
+            StringBuilder retTopRow = new StringBuilder();
+            for (int i = 0; i < topRowChar.Length; i++) {
+                if (Math.IEEERemainder(i + 1, 3).Equals(0) && topRowChar[i].Equals('Y'))
+                    topRowChar[i] = 'R';
+                retTopRow.Append(topRowChar[i]);
+            }
+            return retTopRow;
+        }
+
+        public static string GetHourLights(int pHours) {
+            if (pHours > 24) throw new Exception("Please enter between 0 and 24 Hours");
+            int bottomRow;
+            int topRow = Math.DivRem(pHours, 5, out bottomRow);
+            return SetLights(topRow, 4, 'R') + Environment.NewLine + SetLights(bottomRow, 4, 'R');
+        }
+
+        public static string GetMinuteLights(int pMinutes) {
+            if (pMinutes > 59) throw new Exception("Please enter between 0 and 59 Minutes");
+            int bottomRow;
+            int topRow = Math.DivRem(pMinutes, 5, out bottomRow);
+            return FormatTopRow(topRow) + Environment.NewLine + SetLights(bottomRow, 4, 'Y');
+        }
+
+        public static string SetLights(int pLightsToColor, int pLength, char pColor) {
+            string h = "";
+            h = h.PadLeft(pLightsToColor, pColor);
+            return h.PadRight(pLength, 'O');
+        }
+
+        public static string GetSecondLight(int pSeconds) {
+            if (pSeconds > 59) throw new Exception("Please enter between 0 and 59 for seconds");
+            return Math.IEEERemainder(pSeconds, 2).Equals(0) ? "Y" : "O";
+        }
+
+        public static int[] ParseTime(string pEnterTime) {
+            string[] timeSegments = pEnterTime.Split(':');
+            int[] retVal = new int[3];
+            try {
+                for (int i = 0; i < timeSegments.Length; i++) {
+                    retVal[i] = int.Parse(timeSegments[i]);
+                }
+            }
+            catch (Exception) {
+                throw new Exception("Please enter a time in the hh::mm:ss format ");
+            }
+            return retVal;
+        }
+    }
+}
---
+++ BerlinClock_Tests/UnitTest1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BerlinClock_Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void HandleInputNothingEntered() {
+            const string EXPECTED = "Please enter a time in the hh::mm:ss format ";
+            string actual = BerlinClockKata.BerlinClockKata.HandleInput("");
+            Assert.AreEqual(EXPECTED, actual);
+        }
+
+        [TestMethod]
+        public void HandleInputCorrectInput() {
+            string expected = "Y" + Environment.NewLine + "OOOO" + Environment.NewLine +
+                "OOOO" + Environment.NewLine + "OOOOOOOOOOO" + Environment.NewLine + "OOOO";
+            string actual = BerlinClockKata.BerlinClockKata.HandleInput("00:00:00");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ParseTimeValidInput() {
+            int[] expected = { 11, 11, 11 };
+            int[] actual = BerlinClockKata.BerlinClockKata.ParseTime("11:11:11");
+            for (int i = 0; i < 3; i++) {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Please enter between 0 and 24 Hours")]
+        public void ParseTimeRightLengthWrongFormatInput() {
+            BerlinClockKata.BerlinClockKata.TranslateToBerlinClock("11111:11");
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Please enter a time in the hh::mm:ss format ")]
+        public void ParseTimeAlphaInput() {
+            const string EXPECTED = "Please enter a time in the hh::mm:ss format ";
+            int[] actual = BerlinClockKata.BerlinClockKata.ParseTime("aa:aa:aa");
+            Assert.AreEqual(EXPECTED, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Please enter a time in the hh::mm:ss format ")]
+        public void ParseTimeTooManyColonsException() {
+            const string EXPECTED = "Please enter a time in the hh::mm:ss format ";
+            int[] actual = BerlinClockKata.BerlinClockKata.ParseTime("1:1:1:1:11");
+            Assert.AreEqual(EXPECTED, actual);
+        }
+
+        [TestMethod]
+        public void GetSecondValueOdd() {
+            const string EXPECTED = "O";
+            string actual = BerlinClockKata.BerlinClockKata.GetSecondLight(11);
+            Assert.AreEqual(EXPECTED, actual);
+        }
+
+        [TestMethod]
+        public void GetSecondValueEven() {
+            const string EXPECTED = "Y";
+            string actual = BerlinClockKata.BerlinClockKata.GetSecondLight(12);
+            Assert.AreEqual(EXPECTED, actual);
+        }
+
+        [TestMethod]
+        public void GetSecondValueZero() {
+            const string EXPECTED = "Y";
+            string actual = BerlinClockKata.BerlinClockKata.GetSecondLight(00);
+            Assert.AreEqual(EXPECTED, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Please enter between 0 and 59 for seconds")]
+        public void GetSecondTooBig() {
+            const string EXPECTED = "Please enter between 0 and 59 for seconds";
+            string actual = BerlinClockKata.BerlinClockKata.GetSecondLight(60);
+            Assert.AreEqual(EXPECTED, actual);
+        }
+
+        [TestMethod]
+        public void GetHoursValue13() {
+            string expected = "RROO" + Environment.NewLine + "RRRO";
+            string actual = BerlinClockKata.BerlinClockKata.GetHourLights(13);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetHoursValue03() {
+            string expected = "OOOO" + Environment.NewLine + "RRRO";
+            string actual = BerlinClockKata.BerlinClockKata.GetHourLights(03);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetHoursValue24() {
+            string expected = "RRRR" + Environment.NewLine + "RRRR";
+            string actual = BerlinClockKata.BerlinClockKata.GetHourLights(24);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetHoursValue23() {
+            string expected = "RRRR" + Environment.NewLine + "RRRO";
+            string actual = BerlinClockKata.BerlinClockKata.GetHourLights(23);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Please enter between 0 and 24 Hours")]
+        public void GetHoursValueTooHigh() {
+            const string EXPECTED = "Please enter between 0 and 24 Hours";
+            string actual = BerlinClockKata.BerlinClockKata.GetHourLights(28);
+            Assert.AreEqual(EXPECTED, actual);
+        }
+
+        [TestMethod]
+        public void GetMinutesValue17() {
+            string expected = "YYROOOOOOOO" + Environment.NewLine + "YYOO";
+            string actual = BerlinClockKata.BerlinClockKata.GetMinuteLights(17);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetMinutesValue00() {
+            string expected = "OOOOOOOOOOO" + Environment.NewLine + "OOOO";
+            string actual = BerlinClockKata.BerlinClockKata.GetMinuteLights(00);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetMinutesValue59() {
+            string expected = "YYRYYRYYRYY" + Environment.NewLine + "YYYY";
+            string actual = BerlinClockKata.BerlinClockKata.GetMinuteLights(59);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Please enter between 0 and 59 Minutes")]
+        public void GetMinuteValueTooHigh() {
+            const string EXPECTED = "Please enter between 0 and 59 Minutes";
+            string actual = BerlinClockKata.BerlinClockKata.GetMinuteLights(66);
+            Assert.AreEqual(EXPECTED, actual);
+        }
+
+        [TestMethod]
+        public void TestWholeClock00() {
+            string expected = "Y" + Environment.NewLine + "OOOO" + Environment.NewLine +
+                "OOOO" + Environment.NewLine + "OOOOOOOOOOO" + Environment.NewLine + "OOOO";
+            string actual = BerlinClockKata.BerlinClockKata.TranslateToBerlinClock("00:00:00");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestWholeClock131701() {
+            string expected = "O" + Environment.NewLine + "RROO" + Environment.NewLine +
+                "RRRO" + Environment.NewLine + "YYROOOOOOOO" + Environment.NewLine + "YYOO";
+            string actual = BerlinClockKata.BerlinClockKata.TranslateToBerlinClock("13:17:01");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestWholeClock235959() {
+            string expected = "O" + Environment.NewLine + "RRRR" + Environment.NewLine +
+                "RRRO" + Environment.NewLine + "YYRYYRYYRYY" + Environment.NewLine + "YYYY";
+            string actual = BerlinClockKata.BerlinClockKata.TranslateToBerlinClock("23:59:59");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestWholeClock240000() {
+            string expected = "Y" + Environment.NewLine + "RRRR" + Environment.NewLine +
+                "RRRR" + Environment.NewLine + "OOOOOOOOOOO" + Environment.NewLine + "OOOO";
+            string actual = BerlinClockKata.BerlinClockKata.TranslateToBerlinClock("24:00:00");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
